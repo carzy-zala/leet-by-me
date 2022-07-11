cc483b5375afa9fffac8679bffd6210c99d94f32
crazy-zala <mr.zala2003@gmail.com>
2022-07-11 21:27:46 +0530

    1108 is added in STRING

---
+++ string/1108. Defanging an IP Address.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        
+        string str ="" ;
+        
+        for(int i = 0 ; i < address.length() ; i++){
+            
+            if(address[i]=='.'){
+                str = str + "[.]" ;
+            }
+            
+            else{
+                str = str + address[i] ;
+            }
+        }
+        
+        return str ;
+    }
+};
