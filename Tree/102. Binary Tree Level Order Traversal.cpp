b019f3f0c1668ec829d4ea4b082f72ea02b436e2
crazy-zala <mr.zala2003@gmail.com>
2022-08-27 23:04:11 +0530

    102 is added in TREE

---
+++ Tree/102. Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,41 @@
+class Solution
+{
+public:
+    vector<vector<int>> levelOrder(TreeNode *root)
+    {
+        vector<vector<int>> ans;
+
+        if (root == NULL)
+        {
+            return ans;
+        }
+
+        queue<TreeNode *> q;
+        q.push(root);
+
+        while (!q.empty())
+        {
+            int size = q.size();
+
+            vector<int> a;
+
+            for (int i = 0; i < size; i++)
+            {
+                TreeNode *temp = q.front();
+                q.pop();
+                a.push_back(temp->val);
+
+                if (temp->left)
+                {
+                    q.push(temp->left);
+                }
+                if (temp->right)
+                {
+                    q.push(temp->right);
+                }
+            }
+            ans.push_back(a);
+        }
+        return ans;
+    }
+};
