eb482d90fed762673814dabd83888dff5d011f78
crazy-zala <mr.zala2003@gmail.com>
2022-09-24 20:30:19 +0530

    1688 is added in MATHS

---
+++ maths/1688. Count of Matches in Tournament.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+              
+        if(n==1){
+            return 0;
+        }
+        
+        return n%2==0?n/2+numberOfMatches(n/2):((n-1)/2)+numberOfMatches((n-1)/2+1);
+    }
+};
