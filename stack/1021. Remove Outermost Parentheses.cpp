bb8b8ab7b71c6d61c67429738cda06aadd886ac2
crazy-zala <mr.zala2003@gmail.com>
2022-12-28 08:02:05 +0530

    added in stack

---
+++ stack/1021. Remove Outermost Parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+    stack<char>st;
+    string ans;
+    for(auto a:s)
+    {
+        if(a=='(')
+        {
+            if(st.size()>0)
+            {
+                ans+='(';
+            }
+            st.push('(');
+        }
+        else
+        {
+            if(st.size()>1)
+            {
+                ans+=')';
+            }
+            st.pop();
+        }
+    }
+    return ans;
+    }
+};
